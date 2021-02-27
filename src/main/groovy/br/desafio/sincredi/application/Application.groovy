09cd529ee23b5fd7680234a8ea6578fce4f08e36
brenonardo <brenonardo@hotmail.com>
2021-02-27 20:01:25 -0300

    #s1: Criação das configurações de banco de dados

--- src/main/groovy/br/desafio/sincredi/application/Application.groovy
+++ src/main/groovy/br/desafio/sincredi/application/Application.groovy
@@ -2,8 +2,10 @@ package br.desafio.sincredi.application
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = 'br.desafio.sincredi.application.repository.jpa')
 class Application {
 
 	static void main(String[] args) {
