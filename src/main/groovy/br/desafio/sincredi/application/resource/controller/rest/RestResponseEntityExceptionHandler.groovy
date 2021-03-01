e531dc92130891f29db8619e4e7ef6a6e14020b7
brenonardo <brenonardo@hotmail.com>
2021-03-01 20:56:34 -0300

    #s3: Adicionando tratamento de exceção

--- src/main/groovy/br/desafio/sincredi/application/resource/controller/rest/RestResponseEntityExceptionHandler.groovy
+++ src/main/groovy/br/desafio/sincredi/application/resource/controller/rest/RestResponseEntityExceptionHandler.groovy
@@ -1,6 +1,7 @@
 package br.desafio.sincredi.application.resource.controller.rest
 
 import br.desafio.sincredi.application.exception.SincrediException
+import com.fasterxml.jackson.databind.exc.MismatchedInputException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.http.converter.HttpMessageNotReadableException
@@ -12,15 +13,29 @@ import org.springframework.web.bind.annotation.ResponseStatus
 class RestResponseEntityExceptionHandler {
 
    @ExceptionHandler(value = [HttpMessageNotReadableException.class])
-   @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-   ResponseEntity<Object> handleMessageNotReadableException() {
-      ResponseEntity.badRequest().body('This should be application specific')
+   ResponseEntity<Object> handleMessageNotReadableException(Exception e) {
+      if (e.cause instanceof MismatchedInputException) {
+         def msg = e.cause.message
+         int start = msg.indexOf("'") + 1
+         int idxEnd = start + 1
+         int end = msg.indexOf("'", idxEnd)
+         return ResponseEntity.badRequest().body("Erro ao processar requisição: Campo '${msg.substring(start, end)}'" +
+               " não presente.")
+      }
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body('Erro ao processar o conteúdo da ' +
+            'mensagem.')
    }
 
    @ExceptionHandler(value = [SincrediException.class])
    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
    ResponseEntity<Object> handleSincrediException(Exception e) {
-      ResponseEntity.badRequest().body("Erro ao processar requisição: ${e.message}")
+      ResponseEntity.badRequest().body("Erro ao processar requisição: \"${e.message}\"")
+   }
+
+   @ExceptionHandler(value = [Exception.class])
+   @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+   ResponseEntity<Object> handleException(Exception e) {
+      ResponseEntity.badRequest().body("Erro ao processar a requisição.\nMensagem Original: \"${e.message}\"")
    }
 
 }
