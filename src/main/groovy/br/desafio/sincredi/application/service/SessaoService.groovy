a213afb64cd24e5c953a4d99066cdc5a0305b9ea
brenonardo <brenonardo@hotmail.com>
2021-03-01 21:00:14 -0300

    #s3: Adicionando tratamento de exceção

--- src/main/groovy/br/desafio/sincredi/application/service/SessaoService.groovy
+++ src/main/groovy/br/desafio/sincredi/application/service/SessaoService.groovy
@@ -1,8 +1,9 @@
 package br.desafio.sincredi.application.service
 
 import br.desafio.sincredi.application.dto.to.DuracaoPautaTO
+import br.desafio.sincredi.application.entity.Pauta
 import br.desafio.sincredi.application.entity.Sessao
-import br.desafio.sincredi.application.exception.MissingFieldException
+import br.desafio.sincredi.application.exception.SessaoJaAbertaException
 import br.desafio.sincredi.application.exception.SincrediEntityNotFoundException
 import br.desafio.sincredi.application.mapper.SessaoMapper
 import br.desafio.sincredi.application.repository.jpa.SessaoRepository
@@ -47,21 +48,37 @@ class SessaoService {
 
    def abrir(String sessaoId) throws MissingFieldException, SincrediEntityNotFoundException {
       NullCheckUtil.check(sessaoId)
+      def pauta = this.pautaService.get(sessaoId)
       def sessao = this.repository.findById(IdUtil.uUIDFromString(sessaoId)).orElseThrow {
          new SincrediEntityNotFoundException('Sessao', sessaoId)
       }
+      if (!StatusSessao.AGUARDANDO)
+         throw new SessaoJaAbertaException(sessaoId)
       sessao.status = StatusSessao.ABERTA
       sessao.pauta.dataInscricao = LocalDateTime.now()
       this.repository.saveAndFlush(sessao)
-      this.timeoutSessao(sessao)
+      this.timeoutSessao(sessao, pauta)
    }
 
-   private timeoutSessao(Sessao sessao) {
+   private timeoutSessao(Sessao sessao, Pauta pauta) {
       CompletableFuture.runAsync {
          Thread.sleep(sessao.duracao.toMillis())
          sessao.setStatus(StatusSessao.FINALIZADA)
+         this.contabilizaVotos(sessao, pauta)
          this.repository.saveAndFlush(sessao)
       }
+   }
+
+   private contabilizaVotos(Sessao sessao, Pauta pauta) {
+      int votosAFavor
+      int total
+      sessao.votos.each {
+         total++
+         if (it.aFavor)
+            votosAFavor++
+      }
+      pauta.aprovada = votosAFavor && ((total + 1) / votosAFavor) > (votosAFavor / 2)
+      this.pautaService.save(pauta)
    }
 
 }
